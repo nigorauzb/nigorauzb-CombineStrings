1ec643cde72bb926a0533fa5dfe9ffad5f27df13
nigorauzb <pittsburgh.decor@gmail.com>
2023-02-05 18:18:50 +0000

    commit

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -9,6 +9,9 @@ public class Lab {
      * @return the combination of strings a and b.
      */
     public String combine(String a, String b){
-        return "";
+        String c = (a+b);
+        
+
+        return c;
     }
 }
